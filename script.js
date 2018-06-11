64ac5e63596e18f6345b588bcac197e74c9084e6
Leonel Tejeda <leonelhtejeda@gmail.com>
2018-06-10 20:30:40 -0700

    Invoking instances through the constructor1

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-function plus(a,b) {
-return (
-	console.log(a+b),
-	console.log(this),
-	console.log(arguments)
-)
+function plus(a, b) {
+	return (
+		console.log(a + b),
+		console.log(this),
+		console.log(arguments)
+	)
 };
-plus(5,6);
+plus(5, 6);
 
 
 //using functions as objects
 
 var calc = {
-	status : 'Awesome',
-	plus: function(a,b){
+	status: 'Awesome',
+	plus: function (a, b) {
 		return (
-		console.log(a+b),
-		console.log(this),
-		console.log(arguments),
-		console.log(this.status)
+			console.log(a + b),
+			console.log(this),
+			console.log(arguments),
+			console.log(this.status)
 		)
 	}
 }
-calc.plus(4,5);
+calc.plus(4, 5);
 
 
 //Invoking instances through the constructor
 
-let Dog = function() {
+let Dog = function () {
 	let name, breed;
 	return console.dir(this);
 }
@@ -37,4 +37,3 @@ firstDog.breed = "Doberman";
 secondDog = new Dog;
 secondDog.name = "Nova";
 secondDog.breed = "Yorkie";
-
