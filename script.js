c1d3f777a1cdeb824081dace943bcd5984e79cc1
Leonel Tejeda <leonelhtejeda@gmail.com>
2018-06-10 19:42:59 -0700

    using functions as objects

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ return (
 )
 };
 plus(5,6);
+
+
+//
+
+var calc = {
+	status : 'Awesome',
+	plus: function(a,b){
+		return (
+		console.log(a+b),
+		console.log(this),
+		console.log(arguments),
+		console.log(this.status)
+		)
+	}
+}
+calc.plus(4,5);
