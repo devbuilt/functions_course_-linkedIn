41eed3626a2cfbe277a3f07759dc88cd50c90ecd
Leonel Tejeda <leonelhtejeda@gmail.com>
2018-06-27 23:07:36 -0700

    quick update

---
+++ loops.js
@@ -0,0 +1,8 @@
+let myValue = 0;
+
+while(myValue <100){
+  if (myValue % 9 == 0){
+    console.log(myValue)
+  }
+  myValue++;
+}
--- script.js
+++ script.js
@@ -136,3 +136,52 @@ let ray11 = (function(){
 	};
 
 })();
+
+var countSheep = function (num){
+let val = '';
+for(let i = 1; i <= num; i++){
+	val += `${i} sheep...`;
+}
+return val;
+}
+console.log(countSheep());
+
+
+//
+
+function positiveSum(arr){
+	let total = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > 0) {
+			total += arr[i];
+		}
+
+	}
+	return total;
+}
+positiveSum();
+
+
+function exoticCars(i){
+	let autos = [['Acura'], 'Bmw', 'Lambo', 'Ferrari'];
+	let spacedAutos = "";
+
+	for (let i = 0; i < autos.length; i++) {
+		spacedAutos += autos[i] + '<br>';
+	}
+	document.getElementById('demo').innerHTML = spacedAutos;
+}
+exoticCars();
+
+function getAverage(marks){
+
+	let total = 0;
+for (let i = 0; i < marks.length; i++) {
+	(total += marks[i]);
+
+		}
+	return Math.floor( total / marks.length);
+	}
+
+	console.log(getAverage([3,4,5,6,6,6], 1)
+);
