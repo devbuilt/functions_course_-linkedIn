fa7d31f6344e65b2206f249691b0f03e3eeb2a98
Leonel Tejeda <leonelhtejeda@gmail.com>
2018-07-26 22:18:05 -0700

    ES6 & ES5 ForEach, Filter, Map, Sort Reduce Examples

--- script.js
+++ script.js
@@ -1,187 +1,107 @@
-function plus(a, b) {
-	return (
-		console.log(a + b),
-		console.log(this),
-		console.log(arguments)
-	)
-};
-plus(5, 6);
+const companies = [
+
+    { name: "Company One", category: "Finance", start: "1981", end: "2003" },
+    { name: "Company Two", category: "Retail", start: "1980", end: "2008" },
+    { name: "Company Three", category: "Auto", start: "1987", end: "2007" },
+    { name: "Company Four", category: "Technology", start: "1984", end: "2010" },
+    { name: "Company Five", category: "Finance", start: "2009", end: "2014" },
+    { name: "Company Six", category: "Auto", start: "1995", end: "2003" },
+    { name: "Company Seve", category: "Retail", start: "1986", end: "1996" },
+    { name: "Company Eight", category: "Finance", start: "1988", end: "2016" },
+    { name: "Company Nine", category: "Technology", start: "1987", end: "1989" },
+    
+];
+
+const ages = [13, 45, 60, 24, 46, 27, 21, 25, 10, 11, 22, 17, 11, 19];
+
+// for(let i = 0; i < companies.length; i++ ){
+//     console.log(companies[i])
+
+// }
 
 
-//using functions as objects
+//For Each
+// companies.forEach(function(company){
+// console.log(company.name);
+// });
 
-var calc = {
-	status: 'Awesome',
-	plus: function (a, b) {
-		return (
-			console.log(a + b),
-			console.log(this),
-			console.log(arguments),
-			console.log(this.status)
-		)
-	}
-}
-calc.plus(4, 5);
+/*<------------------------------------------------------- Filter ---------------------------------->*/
+
+// Get 21 or older 
+// let canDrink = [];
+// for(let i = 0; i < ages.length; i++){
+//     if(ages[i] >= 21){
+//         canDrink.push(ages[i]);
+//     }
+// }
+
+//Filter ES5 //Get 21 or older
+// const canDrink = ages.filter(function(age){
+//     if(age >= 21){
+//         return true;
+//     }
+// })
 
 
-//Invoking instances through the constructor
+//Filter ES6 //Get 21 or older
+// const canDrink = ages.filter(age => age >= 21);
+// console.log(canDrink);
 
-let Dog = function () {
-	let name, breed;
-	return console.dir(this);
-}
 
-firstDog = new Dog;
-firstDog.name = 'Roover';
-firstDog.breed = 'Doberman';
+//Fiter Companies ES5 with Retail
+// const RetailCompanies = companies.filter(function (company){
+//     if (company.category === "Retail"){
+//         return true;
+//     }
+// })
 
-secondDog = new Dog;
-secondDog.name = 'Nova';
-secondDog.breed = 'Yorkie';
+// //Filter ES6 Companies with retail
+// const RetailCompanies = companies.filter(company => company.category === "Retail");
 
-//Expanding functionality through prototype
-let speak = function(sayWhat){
-	console.log(sayWhat);
-};
 
-let Dog1 = function () {
-	let name, breed;
+// console.log(RetailCompanies)
 
-};
-let Cat1 = function () {
-	let name, breed;
+// 
+// Filter ES6 Get 1980 Companies 
+// const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990))
+// console.log(eightiesCompanies)
 
-};
 
-Dog1.prototype.speak = speak;
-Cat1.prototype.speak1 = speak;
+// Get companies that lasted 10 years or more
+// const lastedTenYears = companies.filter(company => ( company.end - company.start >= 10  ))
+// console.log(lastedTenYears);
 
-firstDog1 = new Dog1;
-firstDog1.name = 'Roover';
-firstDog1.breed = 'Doberman';
-firstDog1.speak('Woooffff');
 
-firstCat1 = new Cat1;
-firstCat1.name = 'Miles';
-firstCat1.breed = 'Liger';
-firstCat1.speak1('Meow');
-
-//Understanding call-and-apply invocation
-
-let speak1 = function(what){
-	console.log(this.love);
-	console.log(this.normal);
-}
-
-let saySomething = {normal:'meow',  love: 'purr'}
-speak1.call(saySomething);
-//speak.apply(saySomething, ['meouff']); you can pass array wit apply
+/*<------------------------------------------------------- End Filter ---------------------------------->*/
 
 
 
-// Using the arguments parameter
-let plus1 = function(number1,number2) {
-	let sum = 0;
-for (let i = arguments.length -1; i >= 0; i--) {
-	sum += arguments[i];
-	}
-	return sum;
-}
-console.log(plus1(2,2,2,3));
+/*<------------------------------------------------------- Map ---------------------------------->*/
+//create an array of comany names 
+//ES5
+// const companyNames = companies.map(function(company){
+//     return company.name;
+// })
+// console.log(companyNames)
+
+//ES6
+// const companyNames = companies.map(company => company.name)
+// console.log(companyNames)
 
 
-//Using anonymous closures
-var iKnow = function(){
-	console.log('foo');
+// ES5 with ES6 Templeate String
+// const testMap = companies.map(function (company){
+//     return `${company.name}, [${company.start} - ${company.end}]}`;
+// });
+// console.log(testMap)
 
-}();
-
-// self exctuing function
-(function(){
-	console.log('foo');
-})();
-
-//Understanding hoisting and variable scope
-
-function myDog(){
-	var dogName = 'Fido';
-		console.log(dogName + ' says Woooffff');
-}
-myDog();
-
-//Creating and namespacing modules
-
-let ray11 = (function(){
-	let DEFUALTS = {
-		say: 'hello',
-		speed: 'normal'
-
-	}
-	return {
-		speak: function() {
-			let myArguments = arguments[0] || '';
-			let statement = myArguments.say || DEFUALTS.say;
-			console.log(statement);
-			return this;
-
-		},
-		//Chaining module method calls
-		run: function (){
-				let myArguments = arguments[0] || '';
-				let running = myArguments.speed || DEFUALTS.speed;
-				console.log('running...' + running);
-				return this;
-		}
-
-	};
-
-})();
-
-var countSheep = function (num){
-let val = '';
-for(let i = 1; i <= num; i++){
-	val += `${i} sheep...`;
-}
-return val;
-}
-console.log(countSheep());
+// ES6 with ES6 Templeate String
+const testMap = companies.map(company => `${company.name} [${company.start} - ${company.end}]`)
+console.log(testMap)
 
 
-//
-
-function positiveSum(arr){
-	let total = 0;
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] > 0) {
-			total += arr[i];
-		}
-
-	}
-	return total;
-}
-positiveSum();
 
 
-function exoticCars(i){
-	let autos = [['Acura'], 'Bmw', 'Lambo', 'Ferrari'];
-	let spacedAutos = "";
+//Sort
 
-	for (let i = 0; i < autos.length; i++) {
-		spacedAutos += autos[i] + '<br>';
-	}
-	document.getElementById('demo').innerHTML = spacedAutos;
-}
-exoticCars();
-
-function getAverage(marks){
-
-	let total = 0;
-for (let i = 0; i < marks.length; i++) {
-	(total += marks[i]);
-
-		}
-	return Math.floor( total / marks.length);
-	}
-
-	console.log(getAverage([3,4,5,6,6,6], 1)
-);
+//Reduce
